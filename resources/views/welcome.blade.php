0d7c6ca3b42f238dc6a8e59ad4fa55628f4e8e3d
NicolasRecabarrenV <recabarren.valderrama.nicolas@gmail.com>
2020-08-06 19:46:02 -0400

    - Se mejora imagen del chat agregando una imagen de fondo y cambiando el color del navbar. - Se separan algunos estilos en un archivo de css externo.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,86 +11,18 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            #chat p.text-left{
-                margin-bottom: 5px;
-                padding: 4px 120px 4px 6px;
-                border-radius: 7px;
-                position: relative;
-            }
-
-            #chat p.text-left:hover{
-                background-color: #f3f3f3;
-            }
-
-            #chat p.text-left .sended-date{
-                position: absolute;
-                right: 10px;
-                top: 7px;
-                font-size: 12px;
-            }
-        </style>
+        <link rel="stylesheet" href="css/custom.css">
+        
     </head>
     <body>
         <div class="content h-100">
-            <nav class="navbar navbar-light bg-primary position-absolute w-100" style="z-index: 10; top: 0;">
+            <nav class="navbar navbar-light position-absolute w-100" style="z-index: 10; top: 0;">
                 <span class="navbar-brand mb-0 h1 text-light">Chat | Nicolás Recabarren Valderrama</span>
             </nav>
-            <div class="container h-100" style="padding-top: 53px;">
+            <div class="container h-100" style="padding-top: 53px; background: transparent;">
                 <div id="chat-box" class="card p-0 h-100">
 
-                    <div id="chat" class="card-body h-100" style="overflow-y: auto;">
+                    <div id="chat" class="card-body h-100">
                     </div>
 
                     <div id="chat-form" class="card-footer">
